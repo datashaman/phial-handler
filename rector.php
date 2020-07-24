cf316f41b235e7fc3ecb504d989c7366170cfcd5
datashaman <marlinf@datashaman.com>
2020-07-24 06:10:04 +0200

    wip

--- rector.php
+++ rector.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+    $parameters = $containerConfigurator->parameters(
+        [
+            'auto_import_names' => true,
+        ]
+    );
 
     $parameters->set('paths', [__DIR__ . '/src']);
 
