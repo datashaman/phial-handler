99848f3d23a6cd9638e4b43479f0635003a8fa76
datashaman <marlinf@datashaman.com>
2020-08-17 16:40:42 +0200

    wip

--- src/Providers/HandlerServiceProvider.php
+++ src/Providers/HandlerServiceProvider.php
@@ -6,6 +6,8 @@ namespace Datashaman\Phial\Providers;
 
 use Circli\EventDispatcher\EventDispatcher;
 use Circli\EventDispatcher\ListenerProvider\ContainerListenerProvider;
+use Datashaman\Phial\Config;
+use Datashaman\Phial\ConfigInterface;
 use Datashaman\Phial\ContextFactory;
 use Datashaman\Phial\ContextFactoryInterface;
 use Interop\Container\ServiceProviderInterface;
@@ -18,6 +20,26 @@ class HandlerServiceProvider implements ServiceProviderInterface
     public function getFactories()
     {
         return [
+            ConfigInterface::class => function (ContainerInterface $container) {
+                $config = new Config();
+
+                $files = glob(base_dir('config/*.php'));
+
+                if ($files !== false) {
+                    foreach ($files as $filename) {
+                        $name = basename($filename, '.php');
+                        $config->set($name, require($filename));
+                    }
+                }
+
+                $env = getenv('APP_ENV');
+
+                if ($env !== false && file_exists(base_dir("config/env/$env.php"))) {
+                    $config->mergeRecursiveDistinct(require(base_dir("config/env/$env.php")));
+                }
+
+                return $config;
+            },
             ContextFactoryInterface::class => fn(ContainerInterface $container) =>
                 $container->get(ContextFactory::class),
             EventDispatcherInterface::class => fn(ContainerInterface $container) =>
